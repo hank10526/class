b42ba2604505b4bc9eafb2beae2808baf8236984
hank10526 <hank10526@gmail.com>
2026-03-12 10:46:26 +0800

    version 8: PHP

---
+++ hank.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>林憲墉簡介</title>
+	<style type="text/css">
+
+* { font-family:"標楷體"; margin-left:auto; margin-right:auto;}
+
+h1 {color:blue; font-size:60px;}
+
+h2 {color:#11ff1f; font-size:40px;}
+
+
+</style>
+<script>
+		function change1() {
+  			document.getElementById("pic").src = "mountain.jpg";
+  			document.getElementById("h2text").innerText = "靜宜資管";
+		}
+
+		function change2() {
+  			document.getElementById("pic").src = "cliff.jpg";
+  			document.getElementById("h2text").innerText = "林憲墉";
+		}
+	</script>
+
+
+</head>
+<body>
+	<h1>>林憲墉</h1><br>
+`<h2>LIN,SIAN-YONG</h2><br>
+<a href="mailto:hank10526@gmail.com">E-Mail: hank10526@</a>
+<a href="tel:090000000">my phone number</a>
+
+大象席地而坐電影配樂<br>
+
+<audio controls>
+
+<source src="elephant.mp3" type="audio/mP3">
+
+</audio><br>
+
+不要去臺灣<br>
+
+<iframe src="https://www.youtube.com/embed/pW88QFpHXa8" allowfullscreen></iframe>
+
+<table border=10>
+	<tr >
+		<td>
+			1-1
+		</td>
+		<td>
+			1-2
+		</td>
+
+
+	</tr>
+	<tr >
+		<td>
+			2-1
+		</td>
+		<td>
+			2-2
+		</td>
+		<td>
+			2-3
+		</td>
+
+
+	</tr>
+
+
+</table>
+<table width="70%">
+		<tr>
+			<td>
+				<img src="cliff.jpg" width="110%" id="pic" onmouseover="change1()" onmouseout="change2()"><h1></h1></img>
+
+			</td>
+
+			<td>
+				<h1>411322419</h1>
+				<h2 id="h2text">Tzyy-Ching Yang</h2>
+			</td>
+		</tr>
+	</table>
+
+</body>
+<table width="70%" border="1">
+
+<tr>
+
+<td>
+
+<h2>林憲墉</h2><br>
+`<h2>LIN,SIAN-YONG</h2><br>
+<a href="mailto:hank10526@gmail.com">E-Mail: hank10526@</a>
+<a href="tel:090000000">my phone number</a>
+</td>
+
+<td>
+
+大象席地而坐電影配樂<br>
+
+<audio controls>
+
+<source src="elephant.mp3" type="audio/mP3">
+
+</audio><br>
+
+</td>
+
+<td>
+
+不要去臺灣<br>
+
+<iframe src="https://www.youtube.com/embed/pW88QFpHXa8" allowfullscreen></iframe>
+
+</td>
+
+</tr>
+
+</table>
+</html>
+https://github.com/hank10526/class/upload/main
